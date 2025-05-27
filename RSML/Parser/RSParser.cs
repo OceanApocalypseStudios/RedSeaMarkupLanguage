d773a4d4723af54f5db1f96c1e4d3355051bdd5e
MF366 <real_mf366@yahoo.com>
2025-05-27 18:37:24 +0100

    Add project files.

---
+++ RSML/Parser/RSParser.cs
@@ -0,0 +1,15 @@
+﻿namespace RSML.Parser
+{
+
+	public class RSParser
+	{
+
+		protected string returnString = "->";
+		protected string outputString = "||";
+		protected string throwErrorString = "^!";
+
+
+
+	}
+
+}
---
+++ RSML/RSDocument.cs
@@ -0,0 +1,11 @@
+﻿namespace RSML
+{
+
+    public class RSDocument
+    {
+
+
+
+    }
+
+}
