e1cc70c4d606aa2d75ac1654c6de76c437eea007
Matthew <real_mf366@yahoo.com>
2026-06-17 17:16:06 +0100

    Document NativeEvaluationResult struct - Add XML docs for the above mentioned struct

--- src/RSML.Native/Structures/NativeEvaluationResult.cs
+++ src/RSML.Native/Structures/NativeEvaluationResult.cs
@@ -4,14 +4,26 @@
 namespace OceanApocalypseStudios.RSML.Native.Structures
 {
 
+	/// <summary>
+	/// The result of a native evaluation process.
+	/// </summary>
 	[StructLayout(LayoutKind.Sequential)]
 	public struct NativeEvaluationResult
 	{
 
+		/// <summary>
+		/// Whether a match was found. Boolean.
+		/// </summary>
 		public byte wasMatchFound;
 
+		/// <summary>
+		/// The start index for the match value.
+		/// </summary>
 		public int matchValueStart;
 
+		/// <summary>
+		/// The end index for the match value.
+		/// </summary>
 		public int matchValueEnd;
 
 	}
