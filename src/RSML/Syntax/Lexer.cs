10b73eb874a4695151f78ea32d3fc35092047af5
Matthew <real_mf366@yahoo.com>
2026-07-02 19:14:07 +0100

    #49: Prepare project for lexer development in the following days - Add the (so far) empty lexer foundations (`ILexer`, `SyntaxToken` and `TokenKind`) - Add a (so far) empty default implementation of `ILexer` in `Lexer.cs`

---
+++ src/RSML/Syntax/Lexer.cs
@@ -0,0 +1,13 @@
+﻿namespace OceanApocalypseStudios.RSML.Syntax
+{
+
+	internal class Lexer : ILexer
+	{
+
+		// todo: implement ILexer here
+		// thank you thank you thank you
+		// ok ill stop with the fuckass 'thank you' now
+
+	}
+
+}
---
+++ src/RSML/Syntax/TokenKind.cs
@@ -0,0 +1,12 @@
+﻿namespace OceanApocalypseStudios.RSML.Syntax
+{
+
+	public enum TokenKind
+	{
+
+		// todo: add token kinds here
+		// thank you thank you thank you
+
+	}
+
+}
