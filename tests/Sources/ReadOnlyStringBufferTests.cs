d8284962bea2e49147af69e3dc7ece926bc60536
Matthew <real_mf366@yahoo.com>
2026-07-08 15:22:57 +0100

    test(buffer): Test `GetLengthOfLine` method in the read-only string buffer Implement tests for the `GetLengthOfLine` method.

--- tests/Sources/ReadOnlyStringBufferTests.cs
+++ tests/Sources/ReadOnlyStringBufferTests.cs
@@ -37,20 +37,36 @@ public class ReadOnlyStringBufferTests
 	}
 
 	[TestMethod]
-	[DataRow(TestString01, 0, "Hey")]  // H in "Hey"
-	[DataRow(TestString01, 3, "Hey")]  // CR in "Hey\r\n"
-	[DataRow(TestString01, 4, "Hey")]  // LF in "Hey\r\n"
-	[DataRow(TestString01, 5, "This")]  // T in "This"
+	[DataRow(TestString01, 0, 3)] // "Hey"
+	[DataRow(TestString01, 1, 4)] // "This"
+	[DataRow(TestString01, 2, 2)] // "Is"
+	[DataRow(TestString01, 3, 7)] // "A Test "
+	[DataRow(TestString01, 4, 7)] // " Method"
+	[DataRow(TestString01, 5, 0)] // ""
+	[DataRow(TestString01, 6, 1)] // "."
+	[DataRow(TestString01, 7, 0)] // EOF - empty by default
+	public void GetLengthOfLine(string data, int lineNumber, int expectedLength)
+	{
+		var buffer = new ReadOnlyStringBuffer(data);
+		buffer.BuildCache();
+		Assert.AreEqual(expectedLength, buffer.GetLengthOfLine(lineNumber));
+	}
+
+	[TestMethod]
+	[DataRow(TestString01, 0, "Hey")]      // H in "Hey"
+	[DataRow(TestString01, 3, "Hey")]      // CR in "Hey\r\n"
+	[DataRow(TestString01, 4, "Hey")]      // LF in "Hey\r\n"
+	[DataRow(TestString01, 5, "This")]     // T in "This"
 	[DataRow(TestString01, 13, "A Test ")] // A in "A Test"
 	[DataRow(TestString01, 15, "A Test ")] // T in "Test"
 	[DataRow(TestString01, 22, " Method")] // M in "Method"
 	[DataRow(TestString01, 28, " Method")] // First CR in "\r\n\r\n."
 	[DataRow(TestString01, 29, " Method")] // First LF in "\r\n\r\n."
-	[DataRow(TestString01, 30, "")] // Second CR in "\r\n\r\n."
-	[DataRow(TestString01, 31, "")] // Second LF in "\r\n\r\n."
-	[DataRow(TestString01, 32, ".")] // Dot/point in Second LF in "\r\n\r\n."
-	[DataRow(TestString01, 33, ".")] // U2028 in ".\u2028"
-	[DataRow(TestString01, 34, "")] // End of file
+	[DataRow(TestString01, 30, "")]        // Second CR in "\r\n\r\n."
+	[DataRow(TestString01, 31, "")]        // Second LF in "\r\n\r\n."
+	[DataRow(TestString01, 32, ".")]       // Dot/point in Second LF in "\r\n\r\n."
+	[DataRow(TestString01, 33, ".")]       // U2028 in ".\u2028"
+	[DataRow(TestString01, 34, "")]        // End of file
 	public void TryGetLineFromIndex(string data, int index, string expectedLine)
 	{
 		var buffer = new ReadOnlyStringBuffer(data);
