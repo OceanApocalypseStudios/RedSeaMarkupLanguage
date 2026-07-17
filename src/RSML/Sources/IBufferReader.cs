c340dcb08acf328f0c219434f9d209073056fe4c
Matthew <real_mf366@yahoo.com>
2026-07-17 15:30:52 +0100

    chore(build): Mark RSML and RSML.Extensibility projects as AOT compatible
    This way, analyzers that check for trimming, AOT and others are enabled. This commit also disables packing for the benchmarks.
    Refs: #67 #68

--- src/RSML/Sources/IBufferReader.cs
+++ src/RSML/Sources/IBufferReader.cs
@@ -6,10 +6,6 @@ namespace OceanApocalypseStudios.RSML.Sources;
 /// <summary>
 /// Represents a sequential buffer reader whose data is composed of <see cref="Byte"/>s.
 /// </summary>
-/// <remarks>
-/// > [!NOTE]
-/// > Not implemented in v3.0.0-prerelease1.
-/// </remarks>
 public interface IBufferReader : ISource, IEquatable<IBufferReader?>
 {
 	// todo: planned for v3.0.0-prerelease2
