b276577315b21a249cf4e4ec39ef632c84c47883
Matthew <real_mf366@yahoo.com>
2026-07-25 22:49:39 +0100

    docs(native): Add external XML documentation as file to be explicitly included
    I made use of <include> tags to include some external XML documentation.

--- src/RSML.Native/Class1.cs
+++ src/RSML.Native/Class1.cs
@@ -1,5 +1,9 @@
 namespace OceanApocalypseStudios.RSML.Native;
 
+/// <summary>
+/// Placeholder class.
+/// </summary>
+/// <include file="Docs/SharedRemarks.xml" path='docs/members[@name="InstallInstructions"]' />
 public class Class1
 {
 	// todo: remove this placeholder class and export native C functions
