a1c55cf395e1fcc63a25af764d845536d0e854f1
Matthew <real_mf366@yahoo.com>
2026-07-25 15:56:11 +0100

    chore: Setup clean semantic version Reflection-free compile-time attribute

--- src/RSML.CLI/Program.cs
+++ src/RSML.CLI/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Reflection;
 
 
@@ -6,11 +7,35 @@ namespace OceanApocalypseStudios.RSML.CLI;
 
 internal class Program
 {
-	private static void Main(string[] args) =>
-		Console.WriteLine(
-			Assembly.GetExecutingAssembly()
+	private static void Main(string[] args)
+	{
+		// todo: implement
+
+		Console.WriteLine("Assembly Version: " +
+			(Assembly.GetExecutingAssembly()
+				.GetName()
+				.Version
+				?.ToString()
+			?? "Not Found")
+		);
+		Console.WriteLine("File Version: " +
+			(Assembly.GetExecutingAssembly()
 				.GetCustomAttribute<AssemblyFileVersionAttribute>()
 				?.Version
-			?? "Not Found"
-		); // todo
+			?? "Not Found")
+		);
+		Console.WriteLine("Informational Version: " +
+			(Assembly.GetExecutingAssembly()
+				.GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+				?.InformationalVersion
+			?? "Not Found")
+		);
+		Console.WriteLine("Semantic Version: " +
+			(Assembly.GetExecutingAssembly()
+				.GetCustomAttributes<AssemblyMetadataAttribute>()
+				.FirstOrDefault(a => a.Key == "SemVersion")
+				?.Value
+			?? "Not Found")
+		);
+	}
 }
