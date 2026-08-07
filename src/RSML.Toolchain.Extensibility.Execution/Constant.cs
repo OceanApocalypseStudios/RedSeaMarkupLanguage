f28f60f8d6ab3de439c45e474e370be36206359f
Matthew <real_mf366@yahoo.com>
2026-08-07 14:55:34 +0100

    chore: Add XML documentation to `Constant` type

--- src/RSML.Toolchain.Extensibility.Execution/Constant.cs
+++ src/RSML.Toolchain.Extensibility.Execution/Constant.cs
@@ -1,5 +1,8 @@
 namespace OceanApocalypse.RSML.Toolchain.Extensibility.Execution;
 
+/// <summary>
+/// A constant. Its value never changes. It cannot be executed. It can be copied.
+/// </summary>
 public readonly struct Constant
 {
 
